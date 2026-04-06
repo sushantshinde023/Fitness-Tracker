18028f8f01f68fbc5bbbf61757d93f6aafd04fe3
sushantshinde023 <sushantshinde023@gmail.com>
2026-04-06 15:39:28 +0530

    Fitness-Tracker

---
+++ src/main/java/com/project/fitness/FitnessMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.project.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMonolithicApplication.class, args);
+	}
+
+}
